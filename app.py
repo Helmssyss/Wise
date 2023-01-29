3febc4409f22bcd7940095bdd929b49c4556fb2e
Arif <84701901+Arif-Helmsys@users.noreply.github.com>
2023-01-29 21:32:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ class App:
         self.engines = ["google","bing"]
     def __argParse(self):
         argP:ArgumentParser = ArgumentParser(description='How to Using')
-        argP.add_argument("-q","--query","--dork",type=str)
+        argP.add_argument("-q","--query","--dork",type=str,nargs='*')
+        argP.add_argument("-s","--social-media",nargs='*') # yalnızca özenle seçilen sosyal medya da arar
         argP.add_argument("-e","--engine",type=str)
         argP.add_argument("-f","--filter",type=str,help="Özellikle belirtilen url adına işlem yapar")
         argP.add_argument("-px","--proxy",type=str,help="Proxy dosya yolu TxT formatında dosya gerekli.")
@@ -14,9 +15,10 @@ class App:
 
     def run(self):
         __arguments = self.__argParse()
-        if __arguments.query:
+        print(__arguments.query)
+        if (__arguments.query):
             Console.display(Console.BANNER)
-            search = Search(__arguments.query,__arguments.filter) # __arguments.query,__arguments.filter
+            search = Search( __arguments.query,__arguments.filter,__arguments.social_media) # __arguments.query,__arguments.filter
             search.getSearchedLinks()
         else:
             Console.display(f"\n{Console.CYAN}Başlamak için: {Console.PURPLE}python app.py -h")
