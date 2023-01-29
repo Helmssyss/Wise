4805f275bc7a01c891c0dac3b9de12d890203fd5
Arif <84701901+Arif-Helmsys@users.noreply.github.com>
2023-01-29 13:01:45 +0300

    Update const.py

--- pysint/lib/const.py
+++ pysint/lib/const.py
@@ -1,5 +1,13 @@
 from random import choice
 
+def instagram_user_agents():
+    dpi_phone = ['133','320','515','160','640','240','120''800','480','225','768','216','1024']
+    model_phone = ['Nokia 2.4','HUAWEI','Galaxy','Unlocked Smartphones','Nexus 6P','Mobile Phones','Xiaomi','samsung','OnePlus']
+    pxl_phone = ['623x1280','700x1245','800x1280','1080x2340','1320x2400','1242x2688']
+    i_version = ['114.0.0.20.2','114.0.0.38.120','114.0.0.20.70','114.0.0.28.120','114.0.0.0.24','114.0.0.0.41']
+    user_agent = f'Instagram {choice(i_version)} Android (30/3.0; {choice(dpi_phone)}dpi; {choice(pxl_phone)}; huawei/google; {choice(model_phone)}; angler; angler; en_US)'
+    return user_agent
+
 def userAgent() -> str :
     agents = [
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
