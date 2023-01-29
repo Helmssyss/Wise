363e8b8354de2e7aa368fc34aa406921f52de502
Arif <84701901+Arif-Helmsys@users.noreply.github.com>
2023-01-29 17:17:00 +0300

    instagram için user agent eklendi

--- pysint/lib/const.py
+++ pysint/lib/const.py
@@ -30,3 +30,4 @@ HEADER:dict[str,str] = {
     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
     "content-type": "text/html; charset=utf-8"
 }
+    
